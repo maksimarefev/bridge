2b2bed81f6dd1c11fd7140c42f919d6d2b222144
Maksim Arefev <lowwestwins@gmail.com>
2022-04-13 15:52:38 +0300

    Suspended solhint warnings

--- contracts/ERC20MintableBurnable.sol
+++ contracts/ERC20MintableBurnable.sol
@@ -4,6 +4,8 @@ import "./Burnable.sol";
 import "./Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/* solhint-disable no-empty-blocks */
 interface ERC20MintableBurnable is IERC20, Mintable, Burnable {
 
 }
+/* solhint-disable no-empty-blocks */
